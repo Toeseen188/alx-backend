abced84030908982e38ca3139aff5a9b2e1db32f
Hammed Tosin <hammedtosin188@gmail.com>
2023-10-26 08:23:04 +0000

    return None when key is None or not existed

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -30,6 +30,6 @@ class LIFOCache(BaseCaching):
         """
         return the key associate to each item
         """
-        if key is None or item not in self.cache_data:
+        if key is None or key not in self.cache_data:
             return None
         return self.cache_data[key]
