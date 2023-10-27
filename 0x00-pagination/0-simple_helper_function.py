87419e32050ac9aac98bcb317132833b8ea16e9a
Hammed Tosin <hammedtosin188@gmail.com>
2023-10-27 10:35:23 +0000

    first simple helper func

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" pagination """
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int)-> Tuple[int, int]:
+    """
+    takes page and page_size and return a tuple of integerss
+    """
+    end = page * page_size
+    start = end - page_size
+    return (start, end)
