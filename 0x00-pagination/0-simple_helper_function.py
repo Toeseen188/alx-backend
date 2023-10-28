5405e0994843eaaadb72b2cf89804934adb8d2da
Hammed Tosin <hammedtosin188@gmail.com>
2023-10-28 12:20:52 +0000

    remove whitespaces to make pep8

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -3,7 +3,7 @@
 from typing import Tuple
 
 
-def index_range(page: int, page_size: int)-> Tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     takes page and page_size and return a tuple of integerss
     """
