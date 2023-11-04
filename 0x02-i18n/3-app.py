beec8d30799d823eb7ed024a4921dbb9c259fafd
Hammed Tosin <hammedtosin188@gmail.com>
2023-11-04 14:47:01 +0100

    translation and conversion

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+Flask app
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """
+    index page
+    """
+    return render_template('3-index.html', home_title=_('home_title'),
+                           home_header=_('home_header'))
+
+
+class Config:
+    """ this is configuration class to translate lang
+    """
+    LANGUAGES = ["en", "fr"]
+
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel.init_app(app)
+
+
+def get_locale():
+    """
+    get the locale language using request
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == ('__main__'):
+    app.run(debug=False)
