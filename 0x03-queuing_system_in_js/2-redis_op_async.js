340abf1748fa046449340358596a90d2239da37e
Hammed <hammedtosin188@gmail.com>
2024-02-11 14:39:39 +0000

    redis set, get and sethash, gethash

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,41 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err}`);
+});
+
+// Promisify Redis client method
+const getAsync = promisify(client.get).bind(client);
+const setAsync = promisify(client.set).bind(client);
+
+async function setNewSchool(schoolName, value) {
+  await setAsync(schoolName, value)
+    .then((reply) => { 
+    console.log(`Reply: ${reply}`);
+    })
+    .catch((err) => {
+    console.error(err);
+    });
+}
+
+async function displaySchoolValue(schoolName) {
+  await getAsync(schoolName)
+  .then((value) => {
+    console.log(value);
+  })
+  .catch((err) => {
+    console.error('Error:', err);
+  });
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
+
---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,47 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err}`);
+});
+
+function setHash(hashName, key, value) {
+  client.hset(hashName, key, value, (err, reply) => {
+    if(err) {
+      console.error(err);
+    } else {
+      console.log(`Reply: ${reply}`);
+    }
+  });
+}
+
+function getAllHashValue(hashName) {
+  client.hgetall(hashName, (err, reply) => {
+    if (err) {
+     console.error(err);
+    } else {
+      console.log(reply);
+    }
+  });
+}
+setHash('HolbertonSchools', 'Portland', '50');
+setHash('HolbertonSchools', 'Seattle', '80');
+setHash('HolbertonSchools', 'New York', '20');
+setHash('HolbertonSchools', 'Bogota', '20');
+setHash('HolbertonSchools', 'Cali', '40');
+setHash('HolbertonSchools', 'Paris', '2');
+getAllHashValue('HolbertonSchools');
+
+
+
+
+
+
+
+
