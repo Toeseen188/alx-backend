e4909cb69f005b26378c03bcee3b284afe105805
Hammed <hammedtosin188@gmail.com>
2024-02-11 16:23:04 +0000

    job creation using kue

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,27 @@
+import kue from 'kue';
+
+const queue = kue.createQueue();
+
+const push_notification_code = queue.create('Push Notification',
+{
+  phoneNumber: '08155567784',
+  message: 'How are you doing today?',
+});
+
+push_notification_code.on('enqueue', () => {
+  console.log(`Notification job created: ${push_notification_code.id}`);
+});
+
+push_notification_code.on('complete', () => {
+  console.log('Notification job completed');
+});
+
+push_notification_code.on('failed', (err) => {
+  console.error('Notification job failed');
+});
+
+push_notification_code.save((err) => {
+  if (err) {
+    console.error(err);
+  }
+});
